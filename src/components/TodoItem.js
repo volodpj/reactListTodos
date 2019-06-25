e7c3bc35d8a598e6203728a875e5b49e2f380c10
volodpj <volod.af@gmail.com>
2019-06-25 18:08:13 +0300

    cq

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,45 @@
 import React from 'react';
+
+
 class TodoItem extends React.Component{
-    render()
+    constructor(props){
+        super(props)
+    }
+    reqestUser = () => {
+        const reqest = fetch(this.state.urlUsers);
+        reqest.then((response) => {
+          response.json().then((data) => {
+            return data
+            })
+          })
+        } 
+      
+    
+
+    render(){
+
+        return (
+            <table>
+                <tbody>
+                    {this.props.todos.map((todo)=>{
+                    return (
+                        <tr key={todo.id}>
+                            <td>{todo.id}</td>
+                            <td>{todo.title}</td>
+                            <td>
+                                {this.reqestUser().find((user) => {
+                                    return user.id === todo.userId
+                                })}
+                            </td>
+                        </tr>
+                    )})}
+                </tbody>
+                
+            </table>)
+    }
+        
+        
+    
 }
 
 export default TodoItem;

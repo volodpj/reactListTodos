d40b18323e463a3558d672c79a5d80e421cbdd8d
volodpj <volod.af@gmail.com>
2019-06-25 10:03:08 +0300

    Updates

---
+++ precache-manifest.fefb3f558c935690e946793d242a72f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e4ccb86bd0b0d615a9c4f9dcebafb7",
+    "url": "/reactListTodos/index.html"
+  },
+  {
+    "revision": "220c47a0921c26d740d0",
+    "url": "/reactListTodos/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/reactListTodos/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "220c47a0921c26d740d0",
+    "url": "/reactListTodos/static/js/main.a2ff1e65.chunk.js"
+  },
+  {
+    "revision": "332ee50df7e854f0f0c5",
+    "url": "/reactListTodos/static/js/runtime~main.00a63d2c.js"
+  }
+]);
